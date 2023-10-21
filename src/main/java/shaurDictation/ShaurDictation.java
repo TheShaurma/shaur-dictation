70bbc6608c20707694ae8f5873ec7f590efef1cf
TheShaurma <vevrton@mail.ru>
2023-10-21 23:05:27 +0700

    Added some words

--- src/main/java/shaurDictation/ShaurDictation.java
+++ src/main/java/shaurDictation/ShaurDictation.java
@@ -4,7 +4,7 @@ package shaurDictation;
  * Hello world!
  *
  */
-public class App {
+public class ShaurDictation {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
